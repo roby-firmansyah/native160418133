84555b8689115f2e0549c0555a7fb7851e6e06ef
IyanA <s160417159@student.ubaya.ac.id>
2021-08-12 15:22:51 +0700

    Tambah nama Iyan

--- app/src/main/java/com/ubaya/native160418133/MainActivity.kt
+++ app/src/main/java/com/ubaya/native160418133/MainActivity.kt
@@ -7,7 +7,7 @@ import android.widget.Toast
 class MainActivity : AppCompatActivity() {
     private var name:String = "Roby"
     private var phone:String = "081330"
-
+    private var nama:String = "Iyan"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
